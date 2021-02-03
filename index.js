686bc867fcfcefa287c1423c839348f0d71be954
Alexis-Pel <alexispelissier2002@gmail.com>
2021-02-03 16:46:40 +0100

    Tri par ordre alphabétique et export dans un fichier json, mais il manque les retours à la ligne

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 process.argv.shift();
 process.argv.shift();
-console.log(process.argv);
+var moviesTab = require('./movies.json')
 
-for (let index = 0; index < process.argvlength; index++) {
-    if(process.argv[index] == "-action"){
-        if(process.argv[index+1] == "sort_titre"){
+function start(){
+    search_args()
+}
+
+function search_args(){
+    if(process.argv[0] == "-action"){
+        if(process.argv[1] == "sort_titre"){
             sort_titre();
         }
     }
     else{
-        console.log("Erreur, veuillez reéssayer")
+        console.log("Erreur, veuillez réessayer")
     }   
 }
 
-function sort_titre(){
+function sort(tab){
+    for (let i = tab.length-1; i > 1; i--) {
+        for (let j = 0; j < tab.length-1; j++) {
+            let hh = tab[j+1].title.localeCompare(tab[j].title)
+            if(hh == -1){
+                let temp = tab[j+1];
+                tab[j+1] = tab[j];
+                tab[j] = temp;
+            }
+        }
+    }
+    return tab;
+}
+
+function sort_date(){
 
 }
+function sort_titre(){
+    let tab = [];
+    for (let index = 0; index < moviesTab.length; index++) {
+        tab.push(moviesTab[index]);   
+    }
+    tab = sort(tab);
+    tab = JSON.stringify(tab);
+    write("./sort_titre.json", tab )
+}
+
+function write(out, thingToWrite) {
+    let fs = require('fs');
+    fs.writeFile(out, thingToWrite + "\n", function (err) {
+  if (err) return console.log(err);
+});
+}
+start();

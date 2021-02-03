b8e64b575396d77dba63c45ee0e29ab34d6bc8be
Alexis-Pel <alexispelissier2002@gmail.com>
2021-02-03 12:13:02 +0100

    ajout de movies.json dans le dossier

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 process.argv.shift();
 process.argv.shift();
 console.log(process.argv);
+
+for (let index = 0; index < process.argvlength; index++) {
+    if(process.argv[index] == "-action"){
+        if(process.argv[index+1] == "sort_titre"){
+            sort_titre();
+        }
+    }
+    else{
+        console.log("Erreur, veuillez reéssayer")
+    }   
+}
+
+function sort_titre(){
+
+}

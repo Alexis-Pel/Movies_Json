7e40901313f16bd2b19811786637bd4daddbb91c
Alexis-Pel <alexispelissier2002@gmail.com>
2021-02-04 11:00:04 +0100

    ajout output et input

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 process.argv.shift();
 process.argv.shift();
 var moviesTab = require('./movies.json')
+var output = "./output";
+var input = "./movies.json"
 
 function start(){
     search_args()
@@ -8,8 +10,25 @@ function start(){
 
 function search_args(){
     if(process.argv[0] == "-action"){
+        if (process.argv[2]!= input){
+            input = process.argv[2];
+            try {
+                moviesTab = require(input);
+            } catch (error) {
+                console.log("Erreur, le fichier séléctionné n'est pas valide")
+            }
+        }
         if(process.argv[1] == "sort_titre"){
+            if(process.argv[3]!= output){
+                output = process.argv[3];
+            }
             sort_titre();
+        }
+        else if (process.argv[1] == "sort_date"){
+            if(process.argv[3]!= output){
+                output = process.argv[3];
+            }
+            sort_date();
         }
     }
     else{
@@ -31,19 +50,33 @@ function sort(tab){
     return tab;
 }
 
-function sort_date(){
+function sort_date_now(tab){
+    for (let i = tab.length-1; i > 1; i--) {
+        for (let j = 0; j < tab.length-1; j++) {
+            if(tab[j+1].release_date < tab[j].release_date){
+                let temp = tab[j+1];
+                tab[j+1] = tab[j];
+                tab[j] = temp;
+            }
+        }
+    }
+    return tab;
+}
 
+function sort_date(){
+    let tab = [];
+    tab = sort_date_now(moviesTab);
+    for (let index = 0; index < tab.length; index++) {
+        tab[index] = JSON.stringify(tab[index]);
+        write(output, tab[index]);
+    }
 }
 function sort_titre(){
     let tab = [];
-    for (let index = 0; index < moviesTab.length; index++) {
-        tab.push(moviesTab[index]);
-    }
-    tab = sort(tab);
+    tab = sort(moviesTab);
     for (let index = 0; index < tab.length; index++) {
         tab[index] = JSON.stringify(tab[index]);
-        write("./sort_titre.json", tab[index]);
-
+        write(output, tab[index]);
     }
 }
 
